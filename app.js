f999a6bd51d47f81c546612c586af2e16b9b276c
Jonie Miralda <jonie.mirc@gmail.com>
2025-08-01 18:55:55 -0600

    Actualizando el repositorio principal

---
+++ app.js
@@ -0,0 +1,12 @@
+const readline = require('readline');
+
+function suma(a,b){
+    return a+b;
+}
+
+const resta = function(a,b){
+    return a-b;
+}
+
+console.log(suma(5,7));
+console.log(resta(7,5));
